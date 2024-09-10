de26b2c1b425989eb07e2e0d54cbcff1f906c1e7
it1shka <2tishbel@gmail.com>
2024-09-10 17:35:50 +0200

    feat(main): added arrow navigation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser'
-import { appConfig } from './app/app.config'
-import { AppComponent } from './app/app.component'
+import { ApplicationConfig, Component } from '@angular/core'
+import { provideRouter, RouterOutlet, Routes } from '@angular/router'
+import { MainComponent } from './main/main.component'
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<router-outlet />`,
+})
+class AppRoot {}
+
+const routes: Routes = [{ path: '', component: MainComponent }]
+
+const appConfig: ApplicationConfig = Object.freeze({
+  providers: [provideRouter(routes)],
+})
+
+bootstrapApplication(AppRoot, appConfig).catch((err) => console.error(err))
---
+++ src/main/main.component.ts
@@ -0,0 +1,81 @@
+import { NgClass } from '@angular/common'
+import { Component, HostListener } from '@angular/core'
+import { Router, RouterLink } from '@angular/router'
+
+interface Link {
+  href: string
+  title: string
+  description?: string
+  icon?: string
+  external?: boolean
+}
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [RouterLink, NgClass],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+})
+export class MainComponent {
+  constructor (private router: Router) {}
+
+  links: readonly Link[] = Object.freeze([
+    {
+      href: '/level/new',
+      title: 'New Level',
+      description: 'Start creating your own level',
+    },
+    {
+      href: '/library',
+      title: 'Library',
+      description: 'Collection of levels created by other users',
+    },
+    {
+      href: 'https://github.com/it1shka/code-adventure',
+      title: 'GitHub',
+      description: 'Project page on the GitHub. Give it a star!',
+      external: true,
+    },
+  ])
+
+  activeLinkIndex = 0
+
+  openExternalLink = (event: MouseEvent) => {
+    event.preventDefault()
+    if (event.target === null) return
+    if ('href' in event.target && typeof event.target.href === 'string') {
+      const { href } = event.target
+      window.open(href, '_blank')
+    }
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboard = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'Enter': {
+        const { href, external } = this.links[this.activeLinkIndex]
+        if (external) {
+          window.open(href, '_blank')
+        } else {
+          this.router.navigate([href])
+        }
+        break
+      }
+      case 'ArrowUp': {
+        this.activeLinkIndex--
+        if (this.activeLinkIndex < 0) {
+          this.activeLinkIndex = this.links.length - 1
+        }
+        break
+      }
+      case 'ArrowDown': {
+        this.activeLinkIndex++
+        if (this.activeLinkIndex >= this.links.length) {
+          this.activeLinkIndex = 0
+        }
+        break
+      }
+    }
+  }
+}
